2f800038bc43cec839c97f937a9d8301627c2f76
drizzy009 <34764527+drizzy009@users.noreply.github.com>
2023-07-04 09:53:33 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyBRo-DRDavc-u8-H1Xv1CBlIL7nS_JZW7c',
+    messagingSenderId: '413188023608',
+    appId: '1:413188023608:web:32e5a671fc510a00c70dcd',
+    projectId: 'netcore-2d0f3',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96AM68RKF7EHOO2OHA1Q08TPD7LL0',
+    siteid: '303d01c87235c518bbbf52b2d4301eef'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
