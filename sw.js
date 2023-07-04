a4b4d86fc9a27c1d214957cad3b9cd9ef32e8eac
Idris Yusuf <yusid03@gmail.com>
2023-07-04 11:08:52 +0100

    smartec fix

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var config = {
-    apiKey: 'AIzaSyBRo-DRDavc-u8-H1Xv1CBlIL7nS_JZW7c',
-    messagingSenderId: '413188023608',
-    appId: '1:413188023608:web:32e5a671fc510a00c70dcd',
-    projectId: 'netcore-2d0f3',
+    apiKey: 'AIzaSyAaXmGkcQkShBhdj9Do3B1Iw54Fuvn9rHI',
+    messagingSenderId: '656707171996',
+    appId: '1:656707171996:web:2474d086c77365e15cdf59',
+    projectId: 'axon-analytics-7fcc0',
     user_key: 'ADGMOT35CHFLVDHBJNIG50K96AM68RKF7EHOO2OHA1Q08TPD7LL0',
-    siteid: '303d01c87235c518bbbf52b2d4301eef'
+    siteid: '7859e6983fa302a8b4ca5e41cdd9beae'
 };
 importScripts('//cdnt.netcoresmartech.com/swv4.js');
