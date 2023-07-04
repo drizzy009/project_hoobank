905961ead4d1d1d13ffffd1199c320654dcbcfed
Idris Yusuf <yusid03@gmail.com>
2023-07-04 12:59:14 +0100

    sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: 'AIzaSyAaXmGkcQkShBhdj9Do3B1Iw54Fuvn9rHI',
-    messagingSenderId: '656707171996',
-    appId: '1:656707171996:web:2474d086c77365e15cdf59',
-    projectId: 'axon-analytics-7fcc0',
-    user_key: 'ADGMOT35CHFLVDHBJNIG50K96AM68RKF7EHOO2OHA1Q08TPD7LL0',
-    siteid: '7859e6983fa302a8b4ca5e41cdd9beae'
-};
-importScripts('//cdnt.netcoresmartech.com/swv4.js');
